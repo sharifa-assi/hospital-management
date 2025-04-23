d343626bc36cc76365beda9c28f106994f97e5c9
sharifa-assi <sharifaalassi2001@gmail.com>
2025-04-23 21:05:20 +0300

    logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,4 +51,13 @@ class AuthController extends Controller
 
         return redirect('/');
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
 }
